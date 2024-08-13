9d7faee0f9d6f81945d682cc23a701a32a70e4ce
launchKat <katdmerritt@gmail.com>
2024-08-13 17:55:54 -0500

    added sounds to ball collision

--- Pong1.py
+++ Pong1.py
@@ -1,6 +1,6 @@
 #si mple pong game
 import turtle
-import os
+import winsound
 
 wn = turtle.Screen()
 wn.title("Pong by Katie")
@@ -122,7 +122,9 @@ while True:
     if ball.xcor() > -350 and ball.xcor() < -340 and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
+        winsound.PlaySound("bonk-sound.wav", winsound.SND_ASYNC)
 
     if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
+        winsound.PlaySound("bonk-sound.wav", winsound.SND_ASYNC)
